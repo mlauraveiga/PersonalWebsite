9af802e500d3b4c7b0cd61d9bfc0692731ad1957
Laura Veiga <mlauraveiga@gmail.com>
2022-01-07 12:46:53 +0000

    code
    icones, menu, landing page, messy text

--- Code/script.js
+++ Code/script.js
@@ -1,4 +1,4 @@
-//----------------------------- Menu ----------------------------
+//----------------------------- Icones ----------------------------
 // Cursor
 document.body.onmousemove = function(e) {
   document.documentElement.style.setProperty (
@@ -22,7 +22,7 @@ $(document).scroll(function() {
 
     var myScrollFunc = function () {
         var y = window.scrollY;
-        if (y >= 200) {
+        if (y >= window.innerHeight) {
             seta.className = "show"
         } else {
             seta.className = "hide"
@@ -30,6 +30,25 @@ $(document).scroll(function() {
     };
 
     window.addEventListener("scroll", myScrollFunc);
+});
+
+
+//----------------------------- Menu ----------------------------
+// Show menu
+$(document).scroll(function() {
+
+  menu = document.getElementById("menu");
+
+  var scroll = function () {
+      var y = window.scrollY;
+      if (y >= window.innerHeight) {
+          menu.className = "menu_show"
+      } else {
+          menu.className = "menu_hide"
+      }
+  };
+
+  window.addEventListener("scroll", scroll);
 });
 
 
@@ -58,3 +77,34 @@ function change() {
     elem.fadeIn();
   });
 }
+
+
+//----------------------------- Messy Text ----------------------------
+// Rotate Letters
+$(document).scroll(function() {
+
+    m = document.getElementById("messy_m");
+    e = document.getElementById("messy_e");
+    s1 = document.getElementById("messy_s1");
+    s2 = document.getElementById("messy_s2");
+    y = document.getElementById("messy_y");
+
+    var myScrollFunc = function () {
+        var height = window.scrollY;
+        if (height >= window.innerHeight) {
+            m.style.transform = "rotate(18.48deg)";
+            e.style.transform = "rotate(-30.94deg)";
+            s1.style.transform = "rotate(-43.1deg)";
+            s2.style.transform = "rotate(9.07deg)";
+            y.style.transform = "rotate(-28.9deg)";
+        } else {
+          m.style.transform = "rotate(0deg)";
+          e.style.transform = "rotate(0deg)";
+          s1.style.transform = "rotate(0deg)";
+          s2.style.transform = "rotate(0deg)";
+          y.style.transform = "rotate(0deg)";
+        }
+    };
+
+    window.addEventListener("scroll", myScrollFunc);
+});
