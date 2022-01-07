a50cc5538ba411a164f479c9e76199b53c21620b
Laura Veiga <mlauraveiga@gmail.com>
2022-01-07 01:20:26 +0000

    code
    menu, setas

--- Code/script.js
+++ Code/script.js
@@ -1,4 +1,20 @@
 //----------------------------- Menu ----------------------------
+// Cursor
+document.body.onmousemove = function(e) {
+  document.documentElement.style.setProperty (
+    '--x', (
+      e.clientX+window.scrollX
+    )
+    + 'px'
+  );
+  document.documentElement.style.setProperty (
+    '--y', (
+      e.clientY+window.scrollY
+    ) 
+    + 'px'
+  );
+}
+
 // Seta
 $(document).scroll(function() {
 
